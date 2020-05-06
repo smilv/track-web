62f7e4353fd14f7829a06da5e944841ef7833b0b
smilv <owou@icloud.com>
2020-05-06 17:18:29 +0800

    登录

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,39 @@
+/**
+ * 登录模块
+ * bin 2020/5/6
+ */
+import React, { Component } from "react";
+import style from "./style.css";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+class Login extends Component {
+    onFinish = values => {
+        console.log(values);
+    };
+    render() {
+        return (
+            <div>
+                <h3 className={style.title}>登录</h3>
+                <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={this.onFinish}>
+                    <Form.Item name="username" rules={[{ pattern: /^1[\d]{10}$/, required: true, message: "请输入手机号!" }]}>
+                        <Input size="large" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="手机号" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ pattern: /^[0-9A-Za-z]{6,20}$/, required: true, message: "请输入数字、字母组成的6-20位密码!" }]}
+                    >
+                        <Input size="large" prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="密码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" className={style.button}>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -4,12 +4,37 @@
  */
 import React, { Component } from "react";
 import style from "./style.css";
+import LoginComponent from "../../components/Login";
+import RegisterComponent from "../../components/Register";
 
 class Login extends Component {
+    state = {
+        component: 1 //1：登录，2：注册
+    };
+    switch = component => {
+        this.setState({ component });
+    };
     render() {
         return (
             <div className={style.wrapper}>
-                <div className={style.mainBox}></div>
+                <div className={style.mainBox}>
+                    {this.state.component == 1 && (
+                        <div>
+                            <LoginComponent />
+                            <div className={style.switch}>
+                                没有帐号？<a onClick={this.switch.bind(this, 2)}>创建一个</a>
+                            </div>
+                        </div>
+                    )}
+                    {this.state.component == 2 && (
+                        <div>
+                            <RegisterComponent />
+                            <div className={style.switch}>
+                                已有帐号，<a onClick={this.switch.bind(this, 1)}>去登录</a>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
