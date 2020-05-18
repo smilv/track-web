68018217e31a552949861d3a8260de403cf58ea1
smilv <owou@icloud.com>
2020-05-18 20:24:59 +0800

    个人信息

--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -3,17 +3,17 @@
  * @Autor: bin
  * @Date: 2020-05-18 16:38:20
  * @LastEditors: bin
- * @LastEditTime: 2020-05-18 18:29:47
+ * @LastEditTime: 2020-05-18 20:01:02
  */
 import React, { Component } from "react";
-import { Avatar, Form, Input, Button, Row, Col } from "antd";
+import { Avatar, Form, Input, Button, Row, Col, DatePicker } from "antd";
 import style from "./style.css";
 
 class Profile extends Component {
     render() {
         return (
-            <Row>
-                <Col span={8}>
+            <Row gutter={[32, 20]}>
+                <Col span={10}>
                     <Form layout="vertical" className={style.form}>
                         <Form.Item className={style.avatar}>
                             <Avatar size={180} src={`${_tuPath}${global.state.user.avatar}`} />
@@ -22,7 +22,7 @@ class Profile extends Component {
                             <Input placeholder="输入昵称" />
                         </Form.Item>
                         <Form.Item label="出生日期">
-                            <Input placeholder="出生日期" />
+                            <DatePicker style={{ width: "100%" }} />
                         </Form.Item>
                         <Form.Item>
                             <Button type="primary" block>
