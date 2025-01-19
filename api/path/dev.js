fb697db51fa8fd4bbb525ea672778c6c3629f6c5
smilv <owou@icloud.com>
2025-01-20 00:07:39 +0800

    devbox

--- api/path/dev.js
+++ api/path/dev.js
@@ -2,10 +2,10 @@
  * @Description: 域名配置
  * @Autor: bin
  * @Date: 2019-12-25 16:56:36
- * @LastEditors: bin
- * @LastEditTime: 2020-05-18 16:14:14
+ * @LastEditors: zhaobin
+ * @LastEditTime: 2025-01-19 23:56:24
  */
 module.exports = {
-    _apiPath: '"//localhost:3000"',
+    _apiPath: '"https://ietugebkhwee.sealoshzh.site:3000"',
     _tuPath: '"//localhost:3001"'
 };
